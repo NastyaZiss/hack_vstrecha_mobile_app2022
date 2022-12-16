2a7c9bafdcc39d984f7b0730b1bc3367efd58bc3
NastyaZiss <kurbatova.vladuslava050@gmail.com>
2022-12-16 13:17:20 +0300

    error showShachBar

---
+++ lib/snack_bar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SnackBarService {
+  static const errorColor = Colors.red;
+  static const okColor = Colors.green;
+
+  static Future<void> showSnackBar(
+      BuildContext context, String message, bool error
+      ScaffoldMessage.of(context).removeCurrent
+      )
+}
